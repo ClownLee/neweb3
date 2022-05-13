9eccd4c2a54fe9c640b0f3a7dcf725ed0b821550
ele_11_ven <lwentao@hotmail.com>
2022-05-13 10:11:14 +0000

    npm install -g ethereumjs-testrpc

--- controllers/apis/index.js
+++ controllers/apis/index.js
@@ -1,10 +1,14 @@
+const Web3 = require('web3');
+
 module.exports = {
     index (req, res, next) {
         console.log(req.query)
         res.send({ title: 'Express' });
     },
     demo (req, res, next) {
-        res.send('哈哈哈')
+        const web3 = new Web3('ws://localhost:8545')
+        console.log(web3.eth)
+        res.send(web3.eth)
     },
     caonima (req, res, next) {
         res.send({ name: 'Jack Ma', age: 50, sex: 1 })
